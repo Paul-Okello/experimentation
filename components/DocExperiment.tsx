809618a9748725cbc9402e45bed57c2ef454d0cb
Paul Ochieng Okello <paulokello273@gmail.com>
2023-12-12 13:26:14 +0300

    🔨 Refactor Code:  Refactor DocExperiment component and update experiment instructions

--- components/DocExperiment.tsx
+++ components/DocExperiment.tsx
@@ -1,43 +1,53 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import htmlKeyboardResponse from "@jspsych/plugin-html-keyboard-response";
 import { initJsPsych } from "jspsych";
 import "jspsych/css/jspsych.css";
-import jsPsychPreload from "@jspsych/plugin-preload";
 import imageKeyboardResponse from "@jspsych/plugin-image-keyboard-response";
+import { interpretResults, stroopStimuli } from "@/lib/stroop";
 
 const DocExperiment = () => {
   useEffect(() => {
+    const jsPsych = initJsPsych({
+      on_finish: () => {},
+      default_iti: 250,
+    });
+
     const initializeExperiment = () => {
-      const jsPsych = initJsPsych({
-        on_finish: () => {
-          console.log(jsPsych.data.get().values());
-        },
-        show_progress_bar: true,
-        default_iti: 250,
-      });
-
-      const preload = {
-        type: jsPsychPreload,
-        images: ["blue.png", "orange.png"],
-      };
-
       const welcome = {
         type: htmlKeyboardResponse,
-        stimulus: "Welcome to the experiment. Press any key to begin.",
+        stimulus: `
+        <div style="padding:20px;">
+            <h1>Mind Games: Unveiling Your Brain's Colorful Dilemma</h1>
+            <p>This experiment dives into a fascinating puzzle that explores how our brains process information.<br /> By playing with colors and words, it unravels the intricate balance between what we see and what we perceive.<br /> Understanding this experiment can offer insights into how our minds handle conflicting information, shedding light on the complexities of our thought processes.<br /> It's a peek into the magic of our brains and the way they navigate through everyday challenges!
+            </p>
+            <p style="margin:20px;font-size:20px;">Ready to uncover your brain's secret? Press any key on your keyboard to step into the next phase!</p>
+        </div>          
+        `,
       };
 
       const instructions = {
         type: htmlKeyboardResponse,
-        stimulus: ExperimentInstructions,
+        stimulus: `
+          <div style="padding:20px;">
+            <p>In this task you will see a word in a color</p>
+            <p style="color:red;font-size:48px;">BLUE</p>
+            <p>Press r, g, b or y to identify the color, not the word</p>
+            <p>E.g, press r for red in this example</p>
+            <p>Make your responses as quickly and accurately as possible</p>
+            <p>Press any key to begin</p>
+          </div>
+        `,
         post_trial_gap: 2000,
       };
 
-      const test_stimuli = [
-        { stimulus: "blue.png", correct_response: "f" },
-        { stimulus: "orange.png", correct_response: "j" },
-      ];
+      const stroopStimuliFormatted = stroopStimuli.map((stimulus) => ({
+        stimulus: stimulus.stimulus,
+        correct_response: stimulus.correct_response,
+        word: stimulus.word,
+        color: stimulus.color,
+      }));
 
       const fixation = {
         type: htmlKeyboardResponse,
@@ -45,7 +55,7 @@ const DocExperiment = () => {
         choices: "NO_KEYS",
         trial_duration: () => {
           return jsPsych.randomization.sampleWithoutReplacement(
-            [250, 500, 750, 1000, 1250, 1500, 1750, 2000],
+            [1000, 2000, 250, 300, 400, 500, 600, 700, 800, 900],
             1
           )[0];
         },
@@ -54,13 +64,15 @@ const DocExperiment = () => {
         },
       };
 
-      const test = {
-        type: imageKeyboardResponse,
+      const displayStroopItem = {
+        type: htmlKeyboardResponse,
         stimulus: jsPsych.timelineVariable("stimulus"),
-        choices: ["f", "j"],
+        choices: ["r", "g", "b"],
         data: {
-          task: "response",
+          task: "stroop",
           correct_response: jsPsych.timelineVariable("correct_response"),
+          word: jsPsych.timelineVariable("word"),
+          color: jsPsych.timelineVariable("color"),
         },
         on_finish: (data: any) => {
           data.correct = jsPsych.pluginAPI.compareKeys(
@@ -70,36 +82,35 @@ const DocExperiment = () => {
         },
       };
 
-      const test_procedure = {
-        timeline: [fixation, test],
-        timeline_variables: test_stimuli,
-        repetitions: 5,
+      const stroopProcedure = {
+        timeline: [fixation, displayStroopItem],
+        timeline_variables: stroopStimuliFormatted,
+        repetitions: 1,
         randomize_order: true,
       };
 
-      const debrief_block = {
+      const debriefBlock = {
         type: htmlKeyboardResponse,
         stimulus: () => {
-          const trials = jsPsych.data.get().filter({ task: "response" });
-          const correct_trials = trials.filter({ correct: true });
-          const accuracy = Math.round(
-            (correct_trials.count() / trials.count()) * 100
-          );
-          const rt = Math.round(correct_trials.select("rt").mean());
+          const trials = jsPsych.data
+            .get()
+            .filter((item: any) => item.task === "response");
 
-          return `<p>You responded correctly on ${accuracy}% of the trials.</p>
-            <p>Your average response time was ${rt}ms.</p>
+          const correctTrials = trials.filter({ correct: true });
+          const accuracy = Math.round(
+            (correctTrials.count() / trials.count()) * 100
+          );
+          const rt = Math.round(correctTrials.select("rt").mean());
+          const result: string = interpretResults(accuracy, rt);
+
+          return `
+            <p>Thank you for participating in this experiment!</p>
+            <p>${result}</p>
             <p>Press any key to complete the experiment or refresh the page to do another experiment. Thank you!</p>`;
         },
       };
 
-      jsPsych.run([
-        preload,
-        welcome,
-        instructions,
-        test_procedure,
-        debrief_block,
-      ]);
+      jsPsych.run([welcome, instructions, stroopProcedure, debriefBlock]);
     };
 
     if (typeof window !== "undefined") {
@@ -107,51 +118,11 @@ const DocExperiment = () => {
     }
   }, []);
 
-  return <div id='doc-experiment'>DocExperiment</div>;
+  return (
+    <div id='doc-experiment' className='w-full'>
+      DocExperiment
+    </div>
+  );
 };
 
 export default DocExperiment;
-
-const ExperimentInstructions = `
- <div className="custom-container">
-        <div className="custom-text">
-          <p className="custom-text-lg">
-            In this experiment, a circle will appear in the center of the screen.
-          </p>
-          <p className="custom-text-lg">
-            If the circle is
-            <span className="custom-bold custom-text-blue">blue</span>, press the
-            letter <span className="custom-bold">F</span> on the keyboard as
-            fast as you can.
-          </p>
-          <p className="custom-text-lg">
-            If the circle is
-            <span className="custom-bold custom-text-orange">orange</span>,
-            press the letter <span className="custom-bold">J</span> as fast as
-            you can.
-          </p>
-        </div>
-        <div className="custom-flex">
-          <div className="custom-circle mx-4">
-            <img
-              src="blue.png"
-              alt="Blue Circle"
-              className="custom-img"
-            />
-            <p className="custom-text-xs custom-text-blue">
-              Press the <strong>F</strong> key
-            </p>
-          </div>
-          <div className="custom-circle mx-4">
-            <img
-              src="orange.png"
-              alt="Orange Circle"
-              className="custom-img"
-            />
-            <p className="custom-text-xs custom-text-orange">
-              Press the <strong>J</strong> key
-            </p>
-          </div>
-        </div>
-        <p className="custom-text-lg">Press any key to begin.</p>
-      </div>`;
